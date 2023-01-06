29f23ace39b4695921adee3cf34ce64ae1b95649
Andre Trecksel <atrecksel@o365.comhard.de>
2023-01-06 14:17:10 +0100

    Update iliasLogin.js

--- iliasLogin.js
+++ iliasLogin.js
@@ -1,57 +1,65 @@
 /// script to go through the ilias and campus pages
 
-if (window.location.href.indexOf("ilias.studium.kit.edu/login") > -1) {
-  console.log("URL detected");
-  clickButtonById('f807');
-} else if (window.location.href.indexOf("campus.studium.kit.edu") > -1) {
-  console.log("URL detected");
-  setTimeout(function () { clickButtonByClass('shib-login shib-button'); }, 100); //
-} else if (window.location.href.indexOf("idp.scc.kit.edu") > -1) {
-  // Careful, here chrome doesn't load the passwords in properly (Not exactly clear why)
-  // Before clicking on submit, we will use a trick and click on some text beforehand.
+const iURL = window.location.href;
+// cmdClass=ilstartupgui
 
-  // only text, not an actual button. Index = 1 because with Index = 0 
-  // is a class with a button in it which we do not want to click on
-  clickButtonByClass('text full', 1);
-  
-  clickButtonById('sbmt'); // the actual button
-} else if (detectMoreComplexUrl()) {
+if (iURL.indexOf("iliasnet.de/login") > -1) {
   console.log("URL detected");
-  clickListElement("ilTopBarNav", 10)
+  clickButtonByClass('btn btn-default btn-sm');
+  // setTimeout(function () { clickButtonByClass('btn btn-default btn-sm'); }, 1000);
+}
+else {
+  console.log("no URL detected!");
 }
 
-function detectMoreComplexUrl() {
-  // return false for an URL to a forum Link
-  if (window.location.href.toLowerCase().indexOf("https://ilias.studium.kit.edu/ilias.php?baseclass=ilrepositorygui&cmd=viewthread") > -1) {
-    console.log("Forum Link detected, IliasLogin not executing because you should already be logged in.")
-    return false
-  }
+/* * * * */
 
-  // return true for any other URL which links to Magazin but is not a forum URL. If you are already logged in, the login Button is not there so it won't fire
-  if (window.location.href.toLowerCase().indexOf("https://ilias.studium.kit.edu/ilias.php?baseclass=ilrepositorygui") > -1) {
-    console.log("Magazin URL detected, Extensions firering")
-    return true
-  }
-}
+// function detectMoreComplexUrl() {
+//   // return false for an URL to a forum Link
+//   if (window.location.href.toLowerCase().indexOf("https://comhard.iliasnet.de/ilias.php?baseclass=ilrepositorygui&cmd=viewthread") > -1) {
+//     console.log("Forum Link detected, IliasLogin not executing because you should already be logged in.")
+//     return false
+//   }
 
-function clickButtonById(id) {
-  document.getElementById(id).click();
-  console.log("button clicked");
-}
+//   // return true for any other URL which links to Magazin but is not a forum URL. If you are already logged in, the login Button is not there so it won't fire
+//   if (window.location.href.toLowerCase().indexOf("https://comhard.iliasnet.de/ilias.php?baseclass=ilrepositorygui") > -1) {
+//     console.log("Magazin URL detected, Extensions firering")
+//     return true
+//   }
+// }
+
+/* * * * */
+
+// function clickButtonById(id) {
+//   document.getElementById(id).click();
+//   console.log("button clicked:", id);
+// }
+
+/* * * * */
 
 function clickButtonByClass(className, index = 0) {
-  var shibButton = document.getElementsByClassName(className)[index];
-  console.log("button about to be clicked: " + shibButton);
-  shibButton.click();
-  console.log("button clicked");
+  var loginButton = document.getElementsByClassName(className)[index];
+  var inputFields = document.querySelectorAll("input");
+  console.log("button to be clicked: ");
+  console.log(loginButton);
+  console.log("INPUTs: ");
+  console.dir(inputFields);
+  setTimeout(function () {
+    // console.dir(loginButton);
+    loginButton.click();
+  }, 2000);
+  // loginButton.click();
+  // console.log("button clicked:", className);
 }
 
-function clickListElement(listId, elementNumber) {
-  console.log("ClickListElementFct firing")
-  tagNameListElement = "li"
-  urlElement = "a"
-  var listElement = document.getElementById(listId).getElementsByTagName(tagNameListElement)[elementNumber].getElementsByTagName(urlElement)[0]
-  console.log("listElement URL: " + listElement + " about to be clicked")
-  listElement.click()
-  console.log("listElement URL clicked on")
-}
+/* * * * */
+
+// function clickListElement(listId, elementNumber) {
+//   console.log("ClickListElementFct firing")
+//   tagNameListElement = "li"
+//   urlElement = "a"
+//   var listElement = document.getElementById(listId).getElementsByTagName(tagNameListElement)[elementNumber].getElementsByTagName(urlElement)[0]
+//   console.log("listElement URL: " + listElement + " about to be clicked")
+//   listElement.click()
+//   console.log("listElement URL clicked on")
+// }
